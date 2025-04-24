3f0c1e61ecb62e55d59a18e76c8219492cdb63c5
Kien Nguyen Tuan <kiennt2609@gmail.com>
2025-04-24 10:00:27 +0700

    feat(lsp): add lua_ls

---
+++ lua/lsp/servers/lua_ls.lua
@@ -0,0 +1,15 @@
+vim.lsp.enable "lua_ls"
+vim.lsp.config("lua_ls", {
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_markers = {
+        ".luarc.json",
+        ".luarc.jsonc",
+        ".luacheckrc",
+        ".stylua.toml",
+        "stylua.toml",
+        "selene.toml",
+        "selene.yml",
+        ".git",
+    },
+})
